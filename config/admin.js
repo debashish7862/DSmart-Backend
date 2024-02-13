5794b81913fbcad5ec69356daf6c79e3b73d85d6
DebashishSahu <debashish7862@gmail.com>
2024-02-13 10:29:56 +0530

    updated

--- config/admin.js
+++ config/admin.js
@@ -1,29 +1,28 @@
-// module.exports = ({ env }) => ({
-//   auth: {
-//     // secret: env('ADMIN_JWT_SECRET'),
-//     secret: env("0JK2WI3BtrCae9o+b4U8Fw=="),
-//   },
-//   apiToken: {
-//     // salt: env('API_TOKEN_SALT'),
-//     salt: env("PYHXMPv8hnHtxyO81hp5Nw=="),
-//   },
-//   transfer: {
-//     token: {
-//       // salt: env('TRANSFER_TOKEN_SALT'),
-//       salt: env("xw7fmMR71jYoSjkZo4SCOw=="),
-//     },
-//   },
-//   flags: {
-//     nps: env.bool("FLAG_NPS", true),
-//     promoteEE: env.bool("FLAG_PROMOTE_EE", true),
-//   },
-// });
-
-const crypto = require('crypto');
-
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', crypto.randomBytes(16).toString('base64')),
+    // secret: env('ADMIN_JWT_SECRET'),
+    secret: env("0JK2WI3BtrCae9o+b4U8Fw=="),
+  },
+  apiToken: {
+    // salt: env('API_TOKEN_SALT'),
+    salt: env("PYHXMPv8hnHtxyO81hp5Nw=="),
+  },
+  transfer: {
+    token: {
+      // salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("xw7fmMR71jYoSjkZo4SCOw=="),
+    },
+  },
+  flags: {
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
   },
 });
 
+// const crypto = require('crypto');
+
+// module.exports = ({ env }) => ({
+//   auth: {
+//     secret: env('ADMIN_JWT_SECRET', crypto.randomBytes(16).toString('base64')),
+//   },
+// });
